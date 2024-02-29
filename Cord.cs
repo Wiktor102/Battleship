f01ef177448f5d7af9c6cb8b2138d987e2cc66fa
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-02-29 20:56:49 +0100

    Refactor PromptForCord() method

--- Cord.cs
+++ Cord.cs
@@ -21,31 +21,23 @@ namespace Battleship
 
         public static Cord PromptForCord()
         {
-            var valid = false;
-            int i = -1;
-
-            do
-            {
-                i++;
+            for (int i = 0; true; i++) {
                 if (i > 0)
                 {
                     IO.DisplayError("Niepoprawne pole, wybierz ponownie: ", true);
                 }
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                var cordAsString = Console.ReadLine();
-                Console.ForegroundColor = ConsoleColor.White;
-                if (cordAsString.Length != 2 && cordAsString.Length != 3) continue;
+                var cordAsString = IO.ReadLineColored(ConsoleColor.Yellow);
+				if (cordAsString.Length != 2 && cordAsString.Length != 3) continue;
 
-                var letter = cordAsString.Substring(0, 1);
-                if (letter.ToCharArray()[0] - 'A' < 0 || letter.ToCharArray()[0] - 'A' > 10) continue;
+				var letter = cordAsString.Substring(0, 1);
+				if (letter.ToCharArray()[0] - 'A' < 0 || letter.ToCharArray()[0] - 'A' > 10) continue;
 
                 var numberAsString = cordAsString.Substring(1, cordAsString.Length - 1);
-                int num;
-                if (!Int32.TryParse(numberAsString, out num) || num < 1 || num > 10) continue;
+                if (!Int32.TryParse(numberAsString, out int num) || num < 1 || num > 10) continue;
 
                 return new Cord(letter.ToCharArray()[0] - 'A', num - 1);
-            } while (!valid);
+            }
 
             throw new Exception("Something went wrong!");
         }
