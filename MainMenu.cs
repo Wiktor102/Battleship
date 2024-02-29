0f041ac5c580cd54e0d5984279cd50ae18f65c81
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-02-29 14:29:35 +0100

    Move code from Program.cs to new MainMenu class

---
+++ MainMenu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship {
+    internal class MainMenu {
+        Player[] Players = new Player[] { new Player("Gracz 1"), new Player("Gracz 2") };
+
+        public MainMenu() {
+            do {
+                Display();
+            } while (ReadKey());
+        }
+
+        private void Display() {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" $$$$$$\\    $$\\                $$\\     $$\\       $$\\");
+            Console.WriteLine("$$  __$$\\   $$ |               $$ |    $$ |      \\__|");
+            Console.WriteLine("$$ /  \\__|$$$$$$\\    $$$$$$\\ $$$$$$\\   $$ |  $$\\ $$\\");
+            Console.WriteLine("\\$$$$$$\\  \\_$$  _|   \\____$$\\\\_$$  _|  $$ | $$  |$$ |");
+            Console.WriteLine(" \\____$$\\   $$ |     $$$$$$$ | $$ |    $$$$$$  / $$ |");
+            Console.WriteLine("$$\\   $$ |  $$ |$$\\ $$  __$$ | $$ |$$\\ $$  _$$<  $$ |");
+            Console.WriteLine("\\$$$$$$  |  \\$$$$  |\\$$$$$$$ | \\$$$$  |$$ | \\$$\\ $$ |");
+            Console.WriteLine(" \\______/    \\____/  \\_______|  \\____/ \\__|  \\__|\\__|\n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("1. Multiplayer");
+            Console.WriteLine("2. Zagraj z komputerem");
+            Console.WriteLine("3. Ustawienia");
+            Console.WriteLine("4. Legenda");
+            Console.WriteLine("5. Wyjdź");
+        }
+
+        private bool ReadKey() {
+            bool correctKey = false;
+            do {
+                switch (Console.ReadKey().KeyChar) {
+                    case '1':
+                        correctKey = true;
+                        Game.RunGame(Players);
+                        break;
+                    case '2':
+                        correctKey = true;
+                        break;
+                    case '3':
+                        correctKey = true;
+                        break;
+                    case '4':
+                        correctKey = true;
+                        break;
+                    case '5':
+                        return false;
+
+                }
+            } while (!correctKey);
+
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,49 +7,7 @@ using System.Threading.Tasks;
 namespace Battleship {
     internal class Program {
         static void Main(string[] args) {
-            Player[] players = { new Player("Gracz 1"), new Player("Gracz 2") };
-
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine(" $$$$$$\\    $$\\                $$\\     $$\\       $$\\");
-            Console.WriteLine("$$  __$$\\   $$ |               $$ |    $$ |      \\__|");
-            Console.WriteLine("$$ /  \\__|$$$$$$\\    $$$$$$\\ $$$$$$\\   $$ |  $$\\ $$\\");
-            Console.WriteLine("\\$$$$$$\\  \\_$$  _|   \\____$$\\\\_$$  _|  $$ | $$  |$$ |");
-            Console.WriteLine(" \\____$$\\   $$ |     $$$$$$$ | $$ |    $$$$$$  / $$ |");
-            Console.WriteLine("$$\\   $$ |  $$ |$$\\ $$  __$$ | $$ |$$\\ $$  _$$<  $$ |");
-            Console.WriteLine("\\$$$$$$  |  \\$$$$  |\\$$$$$$$ | \\$$$$  |$$ | \\$$\\ $$ |");
-            Console.WriteLine(" \\______/    \\____/  \\_______|  \\____/ \\__|  \\__|\\__|\n");
-            Console.ForegroundColor = ConsoleColor.White;
-
-            Console.WriteLine("1. Multiplayer");
-            Console.WriteLine("2. Zagraj z komputerem");
-            Console.WriteLine("3. Ustawienia");
-            Console.WriteLine("4. Legenda");
-            Console.WriteLine("5. Wyjdź");
-
-            bool correctKey = false;
-            int i = 0;
-            do {
-                switch (Console.ReadKey().KeyChar) {
-                    case '1':
-                        correctKey = true;
-                        Game.RunGame(players);
-                        break;
-                    case '2':
-                        correctKey = true;
-                        break;
-                    case '3':
-                        correctKey = true;
-                        break;
-                    case '4':
-                        correctKey = true;
-                        break;
-                    case '5':
-                        correctKey = true;
-                        break;
-
-                }
-                i++;
-            } while (!correctKey);
+            new MainMenu();
         }
     }
 }
