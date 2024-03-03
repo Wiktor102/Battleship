e66141d975bee1b077c30659a2be825eff996d7f
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-03-03 18:45:52 +0100

    Fix validating ship position

--- Ship.cs
+++ Ship.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,36 +42,44 @@ namespace Battleship {
 			return true;
 		}
 
-		public static Ship Place(Board board, int shipLength, int shipNumber) {
+		public static Ship Place(Board board, int shipSize, int shipNumber) {
 			Cord firstFieldCord;
 			Direction dir;
-			int i = 0;
-			bool correct = true;
 
-			do {
+			for (int i = 0; true; i++) {
 				if (i > 0) {
 					IO.DisplayError("\nWybrałeś niepoprawną pozycję statku!");
 				}
 
-				Console.Write($"Wybierz pole by umieścić statek {shipLength} masztowy nr {shipNumber}: ");
+				Console.Write($"Wybierz pole by umieścić statek {shipSize} masztowy nr {shipNumber}: ");
 				firstFieldCord = Cord.PromptForCord();
-				dir = shipLength == 1 ? Direction.Horizontal : IO.PromptForDirection();
+				dir = shipSize == 1 ? Direction.Horizontal : IO.PromptForDirection();
 
-				for (int j = firstFieldCord.y - 1; j < (dir == Direction.Horizontal ? 3 : firstFieldCord.y + shipLength + 1) && j < 10; j++) {
-					if (j < 0) continue;
-					for (int k = firstFieldCord.x - 1; k < (dir == Direction.Vertical ? 3 : firstFieldCord.x + shipLength + 1) && k < 10; k++) {
-						if (k < 0) continue;
-						if (k > 10 || board.status[j, k] is ShipBoardCell) {
-							correct = false;
-							break;
-						}
+				if (dir == Direction.Horizontal) {
+					if (firstFieldCord.x - 1 >= 0 && board.status[firstFieldCord.y, firstFieldCord.x - 1] is ShipBoardCell) goto next; // Cell to the left of the ship
+					if (firstFieldCord.x + shipSize + 1 < 10 && board.status[firstFieldCord.y, firstFieldCord.x + shipSize + 1] is ShipBoardCell) goto next; // Cell to the right of the ship
+
+					for (int j = firstFieldCord.x - 1; j <= firstFieldCord.x + shipSize; j++) {
+						if (i > 10 || i < 0) continue;
+						if (firstFieldCord.y - 1 >= 0 && board.status[firstFieldCord.y - 1, j] is ShipBoardCell) goto next; // Row above the ship
+						if (firstFieldCord.y + 1 < 10 && board.status[firstFieldCord.y + 1, j] is ShipBoardCell) goto next; // Row below the ship
+					}
+				} else {
+					if (firstFieldCord.y - 1 >= 0 && board.status[firstFieldCord.y - 1, firstFieldCord.x] is ShipBoardCell) goto next; // Cell above the ship
+					if (firstFieldCord.y + shipSize + 1 < 10 && board.status[firstFieldCord.y + shipSize + 1, firstFieldCord.x] is ShipBoardCell) goto next; // Cell below the ship
+
+					for (int j = firstFieldCord.y - 1; j <= firstFieldCord.y + shipSize; j++) {
+						if (i > 10 || i < 0) continue;
+						if (firstFieldCord.x - 1 >= 0 && board.status[j, firstFieldCord.x - 1] is ShipBoardCell) goto next; // Left collumn
+						if (firstFieldCord.x + 1 < 10 && board.status[j, firstFieldCord.x + 1] is ShipBoardCell) goto next; // Right collumn
 					}
 				}
 
-				i++;
-			} while (!correct);
+				break;
+				next: continue;
+			}
 
-			return new Ship(shipLength, firstFieldCord, dir, board);
+			return new Ship(shipSize, firstFieldCord, dir, board);
 		}
 
 	}
